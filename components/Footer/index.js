20e9e8cb2e0bca521aeeb5d030d42aaec063b0f4
kianaamini1 <kamini4@my.bcit.ca>
2024-01-18 16:26:09 -0800

    Exercise class 3

---
+++ components/Footer/index.js
@@ -0,0 +1,8 @@
+import styles from './Footer.module.css'
+export default function() {
+    return(
+        <footer className={styles.footer}>
+            By Kiana
+        </footer>
+    )
+}
---
+++ components/Header/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Header(){
+    return(
+        <header>
+            Header Here
+            <Link href='/'>Home</Link>
+        </header>
+    )
+}
